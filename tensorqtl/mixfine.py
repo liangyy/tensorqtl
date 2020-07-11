5e96ed4d9ec6ead33e3244419bfc7c68ced7ede8
Yanyu Liang <yanyul@uchicago.edu>
2020-07-11 10:01:50 -0500

    dev in mid

--- tensorqtl/mixfine.py
+++ tensorqtl/mixfine.py
@@ -14,10 +14,19 @@ import tensorqtl
 from tensorqtl import genotypeio, cis, SimpleLogger
 import mixqtl
 
+from rpy2.robjects.packages import importr
+r_mixqtl = importr('mixqtl')
+r_base = importr('base')
+from rpy2.robjects import numpy2ri
+numpy2ri.activate()
+import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+
 def finemapper(hap1_t, hap2_t, ref_t, alt_t, raw_counts_t, libsize_t, covariates0_t, 
-               count_threshold=count_threshold, select_covariates=True, , 
-               ase_threshold=ase_threshold, ase_max=ase_max, weight_cap=weight_cap, 
-               mode=mode):
+               count_threshold=100, select_covariates=True, 
+               ase_threshold=50, ase_max=1000, weight_cap=100, 
+               mode='mixfine'):
     
     """
     Inputs:
@@ -37,8 +46,8 @@ def finemapper(hap1_t, hap2_t, ref_t, alt_t, raw_counts_t, libsize_t, covariates
         log_counts_t = raw_counts_t.copy()
         raw_counts_t[:] = count_threshold + 1
     elif mode == 'mixfine' or mode == 'trcfine':
-        log_counts_t = torch.log(raw_counts_t / libsize_t / 2)
-        
+        log_counts_t = torch.log(raw_counts_t / libsize_t / 2).type(torch.float)
+        # breakpoint() 
     mask_t = raw_counts_t >= count_threshold
     mask_cov = raw_counts_t > 0
     
@@ -57,19 +66,81 @@ def finemapper(hap1_t, hap2_t, ref_t, alt_t, raw_counts_t, libsize_t, covariates
     cov_offset[M_cov == False] = 0
     
     if covariates_t.shape[1] != 0:
-        cov_offset[M_cov == True] = regress_out(covariates_t[mask_cov, :], log_counts_t[mask_cov])
+        cov_offset[M_cov == True] = mixqtl.get_offset(covariates_t[mask_cov, :], log_counts_t[mask_cov])
     
     if mode == 'mixfine':
-        res = mixqtl.mixfine(geno1=hap1_t, geno2=hap2_t, 
-                             y1=ref_t, y2=alt_t, 
-                             ytotal=raw_counts_t, 
-                             lib_size=libsize_t, cov_offset=cov_offset)
-    elif mode == 'nefine' or mode == 'trcfine':
-        res = mixqtl.run_susie_default(x=hap1_t + hap2_t, y = log_counts_t - cov_offset)
+        o = {'geno1': hap1_t, 'geno2': hap2_t,
+                             'y1': ref_t, 'y2': alt_t,
+                             'ytotal': raw_counts_t,
+                             'lib_size': libsize_t, 'cov_offset': cov_offset }
+        import pickle 
+        with open('test.pkl', 'wb') as f:
+            pickle.dump(o, f)
+        res = r_mixqtl.mixfine(geno1=hap1_t.T.numpy(), geno2=hap2_t.T.numpy(), 
+                             y1=ref_t.numpy(), y2=alt_t.numpy(), 
+                             ytotal=raw_counts_t.numpy(), 
+                             lib_size=libsize_t.numpy(), cov_offset=cov_offset[:, 0].numpy(),
+                             trc_cutoff=count_threshold, asc_cutoff=ase_threshold, 
+                             asc_cap=ase_max, weight_cap=weight_cap, nobs_asc_cutoff=3)
+    elif mode == 'nefine':
+        res = mixqtl.run_susie_default(x=hap1_t.numpy() + hap2_t.numpy(), y = log_counts_t.numpy() - cov_offset.numpy())
+    elif mode == 'trcfine':
+        cov_offset_ = cov_offset[M, 0]
+        log_counts_t_ = log_counts_t[M]
+        hap1_t_ = hap1_t[M, :]
+        hap2_t_ = hap2_t[M, :]
+        res = mixqtl.run_susie_default(x=hap1_t_.numpy() + hap2_t_.numpy(), y = log_counts_t_.numpy() - cov_offset_.numpy())
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        df_vars = rdf2pd(r_base.summary(res).rx2('vars'))
+        df_cs = rdf2pd(r_base.summary(res).rx2('cs'))
+
+    # re-order df_vars rows by position rather than significance
+    df_ref = pd.DataFrame({'variable': [ i + 1 for i in range(hap1_t.shape[0]) ]})
+    df_vars = pd.merge(df_ref, df_vars, on='variable', how='left')
+    df_vars['variable_idx'] = df_vars['variable'] - 1
+    df_vars = df_vars.drop(columns='variable')
     
-    # TODO
-    return ##
-                        
+    # unpack df_cs
+    df_cs = unpack_cs(df_cs)
+    df_cs['variable_idx'] = df_cs['variable'] - 1
+    df_cs = df_cs.drop(columns='variable')
+    df_cs = pd.merge(df_cs, df_vars[['variable_idx', 'variable_prob']], on='variable_idx')
+
+    return df_vars, df_cs
+
+class CS_collector:
+    def __init__(self):
+        self.base = None
+    def init(self, dict_):
+        if self.base is None:
+            self.base = {}
+            for k in dict_.keys():
+                self.base[k] = []
+    def update(self, dict_):
+        self.init(dict_)
+        for k in dict_.keys():
+            self.base[k].append(dict_[k])
+    def to_df(self):
+        return pd.DataFrame(self.base)
+
+def unpack_cs(df_cs):
+    variable_list = df_cs.variable.to_list()
+    base = CS_collector()
+    for i in range(df_cs.shape[0]):
+        var_str = variable_list[i].split(',')
+        row = df_cs.iloc[i, :].to_dict()
+        for k in var_str:
+            row['variable'] = int(k)
+            base.update(row)
+    return base.to_df()
+
+def rdf2pd(rdf):
+    if r_base.is_null(rdf)[0] is True:
+        return pd.DataFrame({})
+    else:
+        with localconverter(ro.default_converter + pandas2ri.converter):
+            pddf = ro.conversion.rpy2py(rdf)
+        return pddf
 
 def run_mixfine(hap1_df, hap2_df, variant_df, libsize_df, counts_df, ref_df, alt_df,
                 phenotype_pos_df, covariates_df, prefix,
@@ -108,9 +179,9 @@ def run_mixfine(hap1_df, hap2_df, variant_df, libsize_df, counts_df, ref_df, alt
                                  dtype=torch.float32).to(device)
     
     # prepare library size vector
-    libsize_t = torch.from_numpy(libsize_df.values)
+    libsize_t = torch.from_numpy(libsize_df.values[:, 0])
 
-    igm = InputGeneratorMix(hap1_df, hap2_df, variant_df, counts_df, counts_df, 
+    igm = mixqtl.InputGeneratorMix(hap1_df, hap2_df, variant_df, counts_df, counts_df, 
                             ref_df, alt_df, phenotype_pos_df, window=window)
     # iterate over chromosomes
     start_time = time.time()
@@ -142,7 +213,7 @@ def run_mixfine(hap1_df, hap2_df, variant_df, libsize_df, counts_df, ref_df, alt
 
         start = 0
         for k, (raw_counts, _, ref, alt, hap1, hap2, genotype_range, phenotype_id) in enumerate(igm.generate_data(chrom=chrom, verbose=verbose), k+1):
-
+            print(phenotype_id)
             # copy data to GPU
             hap1_t = torch.tensor(hap1, dtype=torch.float).to(device)
             hap2_t = torch.tensor(hap2, dtype=torch.float).to(device)
@@ -160,17 +231,10 @@ def run_mixfine(hap1_df, hap2_df, variant_df, libsize_df, counts_df, ref_df, alt
             tss_distance = np.int32(variant_df['pos'].values[genotype_range[0]:genotype_range[-1]+1] - igm.phenotype_tss[phenotype_id])
             
             res, res_cs = finemapper(hap1_t, hap2_t, ref_t, alt_t, raw_counts_t, libsize_t, covariates0_t, 
-                                     count_threshold=count_threshold, select_covariates=True, , 
+                                     count_threshold=count_threshold, select_covariates=True, 
                                      ase_threshold=ase_threshold, ase_max=ase_max, weight_cap=weight_cap, 
                                      mode=mode)
-            
-            # res_trc, samples_trc, dof_trc = trc_calc(genotypes_t, log_counts_t, raw_counts_t,
-            #                                          covariates0_t, count_threshold=count_threshold, select_covariates=True)
-            # res = [tstat, beta, beta_se, maf, ma_samples, ma_count]
-
-            res_asc, samples_asc, dof_asc = asc_calc(hap1_t, hap2_t, ref_t, alt_t, ase_threshold=ase_threshold, ase_max=ase_max, weight_cap=weight_cap)
-            # res = [tstat_t, beta_t, beta_se_t]
-
+            breakpoint() 
             n = len(variant_ids)
             [tstat_trc, beta_trc, beta_se_trc, maf_trc, ma_samples_trc, ma_count_trc] = res_trc
 
